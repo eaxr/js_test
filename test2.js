e8e21d0306ebc170e78a130e3f82eee2f2c2a00f
eaxr <61225077+eaxr@users.noreply.github.com>
2021-09-17 15:18:08 +0300

    Create test2.js

---
+++ test2.js
@@ -0,0 +1,28 @@
+const fruits = ['Virtual Drives', 'Physical Drives'];
+const text = 'erherhe rhe Virtual Drives : 5 e12135 Virtual Drives   : 9 Physical Drives : 3 sfsfdsgery';
+
+function getProp(text, ary) {
+  var result = [];
+  var obj = [{}];
+  var a = 0;
+  
+  for (let name of ary) {
+    const find = '('+ name + ')\\s+: (\\d+)';
+    const regx =  new RegExp(find, 'g');
+    result[a] = text.match(regx);
+    console.log(result[a]);
+    for (let res of result[a]) {
+      const value = res.match(/\d+/g);
+      obj[name] = value;
+    }
+    a += 1;
+  }
+
+  return obj
+}
+
+var test = getProp(text, fruits);
+
+for (let name of fruits) {
+    console.log(name, "= ", Number(test[name]));
+}
